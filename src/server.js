7a09d554645ac1b08d79e9e72087ea17863f035a
Percy234 <ptduyeta24104@cusc.ctu.edu.vn>
2025-11-10 00:46:21 +0700

    Khởi tạo dự án

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+    console.clear();
+  console.log(`Server is running on http://localhost:${PORT}`);
+})
